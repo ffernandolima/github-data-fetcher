8341e4fe5ca210a039fcf625e987a348987c21d1
Fernando Lima <Fernando.Lima@mindbodyonline.com>
2025-06-04 15:32:05 -0300

    Added support for case-insensitive language name matching in LanguageIconMapper.

--- src/GitHubDataFetcher/Mappers/LanguageIconMapper.cs
+++ src/GitHubDataFetcher/Mappers/LanguageIconMapper.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GitHubDataFetcher.Mappers;
 
 public static class LanguageIconMapper
 {
-    private static readonly Dictionary<string, string> Mappings = new()
-    {
-        { "Python",           "logos-python"        },
-        { "Jupyter Notebook", "logos-jupyter"       },
-        { "HTML",             "logos-html-5"        },
-        { "CSS",              "logos-css-3"         },
-        { "JavaScript",       "logos-javascript"    },
-        { "C#",               "devicon:csharp"      },
-        { "Java",             "logos-java"          },
-        { "Shell",            "simple-icons:shell"  },
-        { "Ruby",             "logos-ruby"          },
-        { "PHP",              "logos-php"           },
-        { "Dockerfile",       "simple-icons:docker" },
-        { "Rust",             "logos-rust"          }
-    };
+    private static readonly Dictionary<string, string> Mappings =
+        new(StringComparer.OrdinalIgnoreCase)
+        {
+            { "Python",           "logos-python"        },
+            { "Jupyter Notebook", "logos-jupyter"       },
+            { "HTML",             "logos-html-5"        },
+            { "CSS",              "logos-css-3"         },
+            { "JavaScript",       "logos-javascript"    },
+            { "C#",               "devicon:csharp"      },
+            { "Java",             "logos-java"          },
+            { "Shell",            "simple-icons:shell"  },
+            { "Ruby",             "logos-ruby"          },
+            { "PHP",              "logos-php"           },
+            { "Dockerfile",       "simple-icons:docker" },
+            { "Rust",             "logos-rust"          }
+        };
 
     public static string GetIconifyIdentifier(string languageName)
         => Mappings.TryGetValue(languageName, out var iconifyIdentifier)
